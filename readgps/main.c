497826371557f6ccbcdecd790247307bcc0b533f
20161104616 <992846873@qq.com>
2017-06-27 10:12:28 +0800

    1.1
    升级系统

--- readgps/main.c
+++ readgps/main.c
@@ -5,10 +5,17 @@ int main(int argc, const char* argv[])
     char a[100];
     char b[100];
     char lat[9],lon[9],spe[6],course[6],day[7],time[7];//lat纬度 lon经度 spe速度 course航向 day年月日 time时间
-    FILE *fr;
+    FILE *fr,*fw;
     int i,n,m,k,timebig,timesmile;//timebig年月日循环变量 timesmile时间循环变量
     int numbig,numsm;//年月日转换变量
     fr = fopen("//Users//a20161104616//Desktop//readgps//GPS170510.log", "r+");
+    if(fr==NULL)
+        printf("fr文件打开失败\n");
+
+    fw = fopen("//Users//a20161104616//Desktop//readgps//GPS.csv", "w+");
+    if(fw==NULL)
+        printf("fw文件打开失败\n");
+
     fscanf(fr, "%s%s",a,b);
     printf("%s\n%s\n",a,b);
     for(i=0;i<8;i++)
@@ -38,10 +45,14 @@ int main(int argc, const char* argv[])
         time[timesmile]=a[timesmile+7];
     time[6]='\0';
     numsm = atoi(time);
-    printf("时间：%d\n",numsm+8000);
     if(numsm>=24000)
     {
-        numsm
+        numsm=numsm-24000;
+        numbig=numbig+10000;
     }
+    printf("时间：%d\n",numsm+8000);
+    fprintf(fw,"%s,%s,%s,%s,%d,%d\n",lat,lon,spe,course,numbig,numsm );
+    fclose(fw);
+    fclose(fr);
     return 0;
 }
